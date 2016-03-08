2321ce60ec8a2fb6bd1a724681a211cf5ccbcd7c
Coyle19 <A_Coyle19@msn.com>
2016-03-08 17:01:09 -0500

    Sound can replay on button push

--- app/src/main/java/s10/shared_virtualdrummer/Sound.java
+++ app/src/main/java/s10/shared_virtualdrummer/Sound.java
@@ -26,6 +26,7 @@ public class Sound extends AppCompatActivity {
 
         Button playbass = (Button) this.findViewById(R.id.play_bass);
         Button playHiHat = (Button) this.findViewById(R.id.play_hi_hat);
+        Button playride = (Button)  this.findViewById(R.id.play_ride);
         Button playsnare = (Button) this.findViewById(R.id.play_snare);
         Button playtom1 = (Button) this.findViewById(R.id.play_tom1);
         Button playtom2 = (Button) this.findViewById(R.id.play_tom2);
@@ -34,36 +35,65 @@ public class Sound extends AppCompatActivity {
         playbass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (bass.isPlaying()){
+                    bass.seekTo(0);
+                }
                 bass.start();
             }
         });
         playHiHat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                hiHat.start();
+                if (hiHat.isPlaying()){
+                    hiHat.seekTo(0);
+                }
+                else {
+                    hiHat.start();
+                }
+            }
+        });
+        playride.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ride.isPlaying()){
+                    ride.seekTo(0);
+                }
+                ride.start();
             }
         });
         playsnare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (snare.isPlaying()){
+                    snare.seekTo(0);
+                }
                 snare.start();
             }
         });
         playtom1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (tom1.isPlaying()){
+                    tom1.seekTo(0);
+                }
                 tom1.start();
             }
         });
         playtom2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (tom2.isPlaying()){
+                    tom2.seekTo(0);
+                }
                 tom2.start();
             }
         });
         playtom3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (tom3.isPlaying()){
+                tom3.seekTo(0);
+            }
                 tom3.start();
             }
         });
