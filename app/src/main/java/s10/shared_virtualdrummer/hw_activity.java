525d3111d5e662a18af66c20b13ee46f4b119de3
Coyle19 <A_Coyle19@msn.com>
2016-03-17 17:42:29 -0400

    Deleted Commented button code

--- app/src/main/java/s10/shared_virtualdrummer/hw_activity.java
+++ app/src/main/java/s10/shared_virtualdrummer/hw_activity.java
@@ -63,25 +63,6 @@ public class hw_activity extends AppCompatActivity {
         drumPlayer = new SoundPlayer(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        /*
-        Button soundMenu = (Button) findViewById(R.id.SoundMenu);
-        soundMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(hw_activity.this, Sound.class);
-                startActivity(intent);
-            }
-        });
-
-        Button menus = (Button) findViewById(R.id.Menus);
-        menus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(hw_activity.this, Menus.class);
-                startActivity(intent);
-            }
-        });
-        */
 
         myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(myBluetoothAdapter == null) {
